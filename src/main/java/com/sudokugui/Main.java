47a3fffb6090f00ba128c7f4e6fd89a79ab8956e
KomlosyT <tamas.komlosy@gmail.com>
2026-01-29 19:47:11 +0100

    add test

--- src/main/java/com/sudokugui/Main.java
+++ src/main/java/com/sudokugui/Main.java
@@ -6,7 +6,7 @@ import javax.swing.SwingUtilities;
  *
  * @author KomlosyT
  */
-
+//test
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(BoardSizeWindow::new);
